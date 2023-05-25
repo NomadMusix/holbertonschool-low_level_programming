96ea689893c6a16aef3ce11edc7a59c2e73d79f6
Alonzo <nomadmusix@gmail.com>
2023-05-24 20:55:43 -0700

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - C program that prints exactly "Programming is like building a multilingual puzzle," 
+ * followed by a new line.
+ *
+ * Return - Always (0)
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle"\n)
+
+	return (0)
+}
