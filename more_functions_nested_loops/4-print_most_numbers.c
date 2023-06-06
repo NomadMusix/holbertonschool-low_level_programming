c7fd49f55d3529c65e4cb0e21c908da9e4548982
Alonzo <nomadmusix@gmail.com>
2023-06-05 20:37:11 -0700

    task

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers prints numbers 0 - 9
+ * print_most_numbers - print base 10 numbers except for 2 & 4
  */
 void print_most_numbers(void)
 {
@@ -8,7 +8,7 @@ void print_most_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != '2' && i != '4);
+		if (i != '2' && i = '4')
 			_putchar(i);
 	}
 	_putchar('\n');
