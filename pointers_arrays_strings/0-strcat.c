a7f0b0cf85191ae3b233594dba24c5de8763df2c
Alonzo <nomadmusix@gmail.com>
2023-06-12 12:52:19 -0700

    5

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strcat - appends src string to dest string
+ * @dest: string to appended
+ * @src: copied string
+ *
+ * Return: pointer to resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (dest[a])
+	{
+		a++;
+	}
+	while (src[b])
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
+}
