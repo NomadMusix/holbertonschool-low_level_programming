16bf9c6663708f692598cd6f169514e1d2c2ec76
Alonzo <nomadmusix@gmail.com>
2023-06-12 12:59:23 -0700

    5

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string.
+ * @dest: destination string
+ * @src: source string
+ * @n:maximum number of characters to be copied from source
+ *
+ * Return: pointer to destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a]; a++)
+		dest[a] = src[a];
+	for ( ; a < n; a++)
+		dest[a] = '\0';
+
+	return (dest);
+}
