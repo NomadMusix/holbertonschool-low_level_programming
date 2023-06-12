c71a2b44bdbeedad64d0a7c9535ae76cd727c055
Alonzo <nomadmusix@gmail.com>
2023-06-12 13:02:00 -0700

    5

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 0 if s1 and s2 are equal, less than 0 if s1 is less than s2
+ * greater than 0 if s1 is greater than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	for (a = 0; s1[a] == s2[a] && s1[a]; a++)
+		;
+
+	return (s1[a] - s2[a]);
+}
