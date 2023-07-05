95552ab159188f613c4227c26fde793ca2ebdcce
Alonzo <nomadmusix@gmail.com>
2023-07-04 21:03:10 -0700

    TASK

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);  /* prints the name of the current file */
+	return (0);
+}
