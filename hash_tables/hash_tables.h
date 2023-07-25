9120dd012617c9b26a34014c35248f3eed750df4
Alonzo <nomadmusix@gmail.com>
2023-07-24 21:20:04 -0700

    commit

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,44 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+/**
+ * struct hash_node_s - A structure to store key-value pairs in the hash
+ * table
+ * @key: The key, a string
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the list
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+/**
+ * struct hash_table_s - A structure to store the hash table and its size
+ * @size: The size of the array
+ * @array: An array of size @size
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+/* Function to create a hash table */
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+void free_node(hash_node_t *node);
+
+#endif /* HASH_TABLES_H */
